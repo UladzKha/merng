77247066a22092d5b9912660dc756ad9bab11d31
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2020-12-29 17:56:16 +0100

    user registration and login, validation input

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB:
-      "mongodb+srv://<username>:<password>@cluster0.qdqlf.mongodb.net/merng?retryWrites=true&w=majority",
-  };
-  
+  MONGODB:
+    "mongodb+srv://<username>:<password>@cluster0.qdqlf.mongodb.net/merng?retryWrites=true&w=majority",
+    SECRET_KEY: 'some key should be here'
+};
--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
-const Post = require("./models/Post");
-const User = require("./models/User");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
@@ -38,7 +11,7 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => console.log(err))
   .then(() => {
     console.log("Mongodb connected");
