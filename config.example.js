9b1e1f62298ac47518b9c50ba8c18b28694598ea
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2021-01-03 15:22:32 +0100

    post mutations

--- config.example.js
+++ config.example.js
@@ -1,5 +1,3 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://<username>:<password>@cluster0.qdqlf.mongodb.net/merng?retryWrites=true&w=majority",
-    SECRET_KEY: 'some key should be here'
-};
+export const MONGODB =
+  "mongodb+srv://<username>:<password>@cluster0.qdqlf.mongodb.net/merng?retryWrites=true&w=majority";
+export const SECRET_KEY = "some key should be here";
---
+++ util/check_auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { AuthenticationError } from "apollo-server";
+
+import { SECRET_KEY } from "../config.js";
+
+const checkAuth = (context) => {
+  const authHeader = context.req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split("Bearer ")[1];
+
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid/Expired token");
+      }
+    }
+
+    throw new Error("Authentication token must be Bearer [token]");
+  }
+  throw new Error("Authorization header must be provied");
+};
+
+export default checkAuth;
