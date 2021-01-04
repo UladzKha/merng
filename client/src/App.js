c11264f678c2b181c71f0f3ce86f9a762dbb9fe5
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2021-01-04 18:10:41 +0100

    client part, semantic ui, react-router, apollo-provider

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
